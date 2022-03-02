628ac15ad31ba241ddc8872676918d7ac463c397
epolichr <polichronakis.e@northeastern.edu>
2022-03-02 13:03:17 -0500

    Added temporary table

--- view/leaderboard_tkinter_view.py
+++ view/leaderboard_tkinter_view.py
@@ -1,22 +1,64 @@
-import mysql.connector
 import tkinter as tk
-from tkinter import *
-window = tk.Tk()
-window.geometry("400x250")
- my_connect = mysql.connector.connect(
-   host="localhost",
-   user="userid",
-   passwd="password",
-   database="database_name"
- )
- my_conn = my_connect.cursor()
- ####### end of connection ####
- my_conn.execute("SELECT * FROM student limit 0,10")
-i=0
-for student in my_conn:
-    for j in range(len(student)):
-        e = Entry(window, width=10, fg='blue')
-        e.grid(row=i, column=j)
-        e.insert(END, student[j])
-    i=i+1
-window.mainloop()
+from tkinter import ttk
+from main_menu import MainMenu
+
+bg_1 = "#E0FBFC"
+bg_2 = "#C2DFE3"
+cor_1 = "#9DB4C0"
+incor_1 = "#5C6B73"
+fg_1 = "#253237"
+
+
+class LeaderboardView(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        self.leaderboard_title = tk.Label(text="Leaderboard", bg=bg_2, width="300", height="1",
+                                          font=("Calibri", 11, "bold"), fg=fg_1, borderwidth=4, relief="groove",
+                                          pady="10")
+        self.leaderboard_title.pack()
+        self.main_button = tk.Button(text='Main Menu', command=self.button_main)
+        self.main_button.pack()
+
+        self.leaderboard = ttk.Treeview()
+
+        self.leaderboard['columns'] = ('rank', 'player_Name', 'elo')
+        self.leaderboard.column("#0", width=0, stretch=False)
+        self.leaderboard.column("rank", anchor=tk.CENTER, width=80)
+        self.leaderboard.column("player_Name", anchor=tk.CENTER, width=80)
+        self.leaderboard.column("elo", anchor=tk.CENTER, width=80)
+
+        self.leaderboard.heading("#0", text="", anchor=tk.CENTER)
+        self.leaderboard.heading("rank", text="Rank", anchor=tk.CENTER)
+        self.leaderboard.heading("player_Name", text="Player Name", anchor=tk.CENTER)
+        self.leaderboard.heading("elo", text="ELO", anchor=tk.CENTER)
+
+        self.leaderboard.insert(parent='', index='end', iid=0, text='',
+                                values=('1', 'john', '00'))
+        self.leaderboard.insert(parent='', index='end', iid=1, text='',
+                                values=('2', 'john', '00'))
+        self.leaderboard.insert(parent='', index='end', iid=2, text='',
+                                values=('3', 'john', '00'))
+        self.leaderboard.insert(parent='', index='end', iid=3, text='',
+                                values=('4', 'john', '00'))
+        self.leaderboard.insert(parent='', index='end', iid=4, text='',
+                                values=('5', 'john', '00'))
+        self.leaderboard.insert(parent='', index='end', iid=5, text='',
+                                values=('6', 'john', '00'))
+        self.leaderboard.insert(parent='', index='end', iid=6, text='',
+                                values=('7', 'john', '00'))
+        self.leaderboard.insert(parent='', index='end', iid=7, text='',
+                                values=('8', 'john', '00'))
+        self.leaderboard.insert(parent='', index='end', iid=8, text='',
+                                values=('9', 'john', '00'))
+        self.leaderboard.insert(parent='', index='end', iid=9, text='',
+                                values=('10', 'john', '00'))
+
+        self.leaderboard.pack()
+        # show the frame on the container
+        self.pack()
+
+    def button_main(self):
+        button_clicked_label = tk.Label(text="button clicked", bg=bg_2, width="300", height="1",
+                                        font=("Calibri", 11, "bold"), fg=fg_1, borderwidth=4, relief="groove",
+                                        pady="10")
+        button_clicked_label.pack()
---
+++ view/main_menu.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import ttk
+
+bg_1 = "#E0FBFC"
+bg_2 = "#C2DFE3"
+cor_1 = "#9DB4C0"
+incor_1 = "#5C6B73"
+fg_1 = "#253237"
+
+
+class MainMenu(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        self.new_game_button = tk.Button(text="New Game", height="1", width="12", command=self.new_game, bg=bg_2, fg=fg_1,
+                                    font=("Calibri", 18, "bold"), activebackground=cor_1)
+        self.new_game_button.pack()
+        self.settings_button = tk.Button(text="Settings", height="1", width="12", command=self.new_game, bg=bg_2, fg=fg_1,
+                                    font=("Calibri", 18, "bold"), activebackground=cor_1)
+        self.settings_button.pack()
+
+        self.leaderboard_button = tk.Button(text="Leaderboard", height="1", width="12", command=self.new_game, bg=bg_2, fg=fg_1,
+                                       font=("Calibri", 18, "bold"), activebackground=cor_1)
+        self.leaderboard_button.pack()
+        self.logout_button = tk.Button(text="Login", height="1", width="12", command=self.new_game, bg=bg_2, fg=fg_1,
+                                  font=("Calibri", 18, "bold"), activebackground=cor_1)
+        self.logout_button.pack()
+
+        # show the frame on the container
+        self.pack()
+
+    def new_game(self):
+        button_clicked_label = tk.Label(text="button clicked", bg=bg_2, width="300", height="1",
+                                        font=("Calibri", 11, "bold"), fg=fg_1, borderwidth=4, relief="groove",
+                                        pady="10")
+        button_clicked_label.pack()
