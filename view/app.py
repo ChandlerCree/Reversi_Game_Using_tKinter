93aeb6489fd2d22a62288040085bd48ef8ddfc53
ChandlerCree <chandlercreegk@gmail.com>
2022-03-12 13:53:55 -0500

    Various UI changes, disabled new game button until game settings are saved, added functionality for logout/login on login button, added current user and current elo label

--- view/app.py
+++ view/app.py
@@ -20,39 +20,40 @@ class App(tk.Tk):
     board_size = "6"
     p1 = '#000000'
     p2 = '#FFFFFF'
-    user_name = "Guest"
-    user_elo = 1500
-    user_info = "Welcome {}!\nCurrent Elo: {}".format(user_name, str(user_elo))
+    default_username = "Guest"
+    default_elo = 0
+    logged_user = ""
+    logged_elo = 0
+    ng_btn_stat = "disabled"
 
 
     def __init__(self):
         super().__init__()
 
         self.title('Reversi')
-        self.geometry('300x410')
+        #self.geometry('300x410')
         self.configure(bg=bg_1)
         self.resizable(False, False)
 
         # label
-        title_label = tk.Label(text="REVERSI", bg=bg_2, width="300", height="1", font=("Calibri", 24, "bold"), fg=fg_1,
-                               borderwidth=4, relief="groove", pady="10")
+        title_label = tk.Label(text="REVERSI", bg=bg_2, width="20", height="1", font=("Calibri", 24, "bold"), fg=fg_1,
+                                            borderwidth=4, relief="groove", pady="10")
         title_label.pack()
 
         self.frame = tk.Frame(self)
         self.frame.configure(bg=bg_1)
 
-        self.username_label = tk.Label(self.frame, text=self.user_info, height="2", font=("Calibri", 12), 
+        self.username_label = tk.Label(self.frame, text="Welcome {}!\nCurrent Elo: {}".format(self.default_username, str(self.default_elo)), height="2", font=("Calibri", 12, "italic"), 
                                             bg=bg_1, fg=fg_1)
         self.username_label.pack(pady=8)
 
         self.new_game_button = tk.Button(self.frame, text="New Game", height="1", width="12", command=self.new_game,
-                                         bg=bg_2, fg=fg_1,
-                                         font=("Calibri", 18, "bold"), activebackground=cor_1)
+                                            bg=bg_2, fg=fg_1, font=("Calibri", 18, "bold"), activebackground=cor_1, state="disabled")
         self.new_game_button.pack(pady=8)
 
-        self.settings_button = tk.Button(self.frame, text="Settings", height="1", width="12",
-                                         command=self.open_settings, bg=bg_2, fg=fg_1,
-                                         font=("Calibri", 18, "bold"), activebackground=cor_1)
+        self.settings_button = tk.Button(self.frame, text="Game Settings", height="1", width="12",
+                                            command=self.open_settings, bg=bg_2, fg=fg_1,
+                                            font=("Calibri", 18, "bold"), activebackground=cor_1)
         self.settings_button.pack(pady=8)
 
         self.leaderboard_button = tk.Button(self.frame, text="Leaderboard", height="1", width="12",
@@ -61,14 +62,24 @@ class App(tk.Tk):
         self.leaderboard_button.pack(pady=8)
         
         self.login_button = tk.Button(self.frame, text="Login", height="1", width="12", command=self.open_login,
-                                       bg=bg_2, fg=fg_1,
-                                       font=("Calibri", 18, "bold"), activebackground=cor_1)
-        self.login_button.pack(pady=8)
+                                            bg=bg_2, fg=fg_1, font=("Calibri", 18, "bold"), activebackground=cor_1)
+        self.login_button.pack(pady=(8, 24))
 
         # show the frame on the container
         self.frame.pack()
 
+    def change_username_label(self):
+        self.username_label.configure(text="Welcome {}!\nCurrent Elo: {}".format(self.logged_user, self.logged_elo))
+        if self.logged_user != "Guest":
+            self.login_button.configure(text="Logout")
+        else:
+            self.login_button.configure(text="Login")
+
+    def change_newgame_state(self):
+        self.new_game_button.configure(state="normal")
+
     def new_game(self):
+        print(self.board_size)
         game = Game(size=int(self.board_size))  # create the game
 
         game_win = GUIView(self.master, game.board)
@@ -85,7 +96,9 @@ class App(tk.Tk):
         self.withdraw()
 
     def open_leaderboard(self):
-        leaderboard_win = LeaderboardView(self.master)
+        login_controller = ConnectorController()
+        self.my_conn = login_controller.connect_mysql()
+        leaderboard_win = LeaderboardView(self.master, self.my_conn)
         leaderboard_win.focus_force()
         self.withdraw()
 
